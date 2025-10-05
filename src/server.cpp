75a80c0a9a026019e01f10246b29451603ff9682
hindshujaul <43566458+hindshujaul@users.noreply.github.com>
2025-10-05 16:50:26 +0530

    persistant connection

--- src/server.cpp
+++ src/server.cpp
@@ -195,6 +195,7 @@ void process_request(int clientsocket,string directory,string request)
 			int start=spos+to_find_file.length();
 			int end=request.length();
 			string client_str=request.substr(start,end-start);
+			cout<<"CLIENT STR"<<client_str<<endl;
 			//Write to file
 			if(writeToFile(client_str,fullpath))
 			{
